f3704fb207ad699b41718ee489c2ed97a8bfb177
Manuel Schwarz <manschwa@uos.de>
2023-04-27 18:18:47 +0200

    refactoring code

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -2,14 +2,17 @@ import time
 import qrcode
 from math import floor
 
+studip_base_url = "https://studip.uni-osnabrueck.de"
+default_duration = 3600
+
 filename = "rooms.txt"
 with open(filename) as file:
     rooms = file.read().splitlines()
 
 current_time = floor(time.time())
-end_booking = current_time + 3600
+end_booking = current_time + default_duration
 
 for room in rooms:
-    booking_url = 'https://studip.uni-osnabrueck.de/dispatch.php/resources/booking/add/{}?resource_id={}&begin={}&end={}'.format(room, room, current_time, end_booking)
+    booking_url = '{}/dispatch.php/resources/booking/add/{}?resource_id={}&begin={}&end={}'.format(studip_base_url, room, room, current_time, end_booking)
     img = qrcode.make(booking_url)
     img.save("qr_code_{}.png".format(room))
